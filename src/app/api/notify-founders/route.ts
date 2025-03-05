dbd679fba841ede4d68f3c54cc9c3fc6dc409a25
anuar kapbasov <127466831+anu-mdl@users.noreply.github.com>
2025-03-05 18:42:03 +0500

    Update route.ts

--- src/app/api/notify-founders/route.ts
+++ src/app/api/notify-founders/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
         },
         body: JSON.stringify({
           messaging_product: "whatsapp",
-          to: "787021544205",
+          to: "787058275283",
           type: "template",
           template: {
             name: "consultation_notify",
